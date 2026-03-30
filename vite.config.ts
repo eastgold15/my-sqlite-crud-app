b31edce785391e0988a9a1dbd8708a79d82d7ee6
PoRi <1960825664@qq.com>
2026-03-30 11:25:03 +0800

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path"
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   root: "src/mainview",
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/mainview"),
+    },
+  },
   build: {
     outDir: "../../dist",
     emptyOutDir: true,
